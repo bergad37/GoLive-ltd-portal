9ec7e2e762b99b7175df84cd132c22af806bf04e
bergad37 <bergad37@gmail.com>
2025-05-27 17:07:47 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+@app.get("/")
+async def read_root():
+    return 'Here is my first FastApi request'
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
